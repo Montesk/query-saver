62b8c9ff368af82a56b582dc6d34564d6dbd40df
Aleksey Spirin <a.spirin@s-e.su>
2020-01-22 02:11:23 +0300

    - reorganize project structure - add storage types const - add session storage support - add options support - add todo for next iteration plugin extension - refactoring

--- static/script.js
+++ static/script.js
@@ -1,28 +1,51 @@
-function utmSaver() {
-    // CONSTANTS
-    var storageKey = "mt_utm_saver"
+// CONSTANTS
+const storageKey = "mt_utm_saver"
 
-    // MODELS
-    function utmStorageModel() {
+// TYPES
+const storageTypes = {
+    sessionStorage: 1,
+    localStorage : 2,
+}
+
+/**
+ * Models
+ * @type {{newStorageModel: (function(): {createdAt: string, query: null})}}
+ */
+const models = {
+    newStorageModel: function () {
         return {
             query: null,
             createdAt: "",
         }
     }
-
-    // WORKING WITH QUERY
-    function hasQuery(searchQuery) {
+}
+/**
+ * Module to work with search query and model
+ * @type {{parseQueryToObject: queryModule.parseQueryToObject, parseObjectToQuery: (function(*): string), hasQuery: queryModule.hasQuery}}
+ */
+const queryModule = {
+    /**
+     * Checks if location has search query
+     * @param searchQuery
+     * @returns {boolean}
+     */
+    hasQuery: function (searchQuery) {
         if (searchQuery === "") {
             return false
         }
 
         return true
-    }
+    },
 
-    function parseQueryToObject(queryString) {
-        var result = null
+    /**
+     * parse query string to query model
+     * @param queryString
+     * @returns {null|*}
+     */
+    parseQueryToObject: function (queryString) {
+        let result = null
 
-        if (!hasQuery(queryString)) return null
+        if (!this.hasQuery(queryString)) return null
 
         try {
             result = JSON.parse('{"' + queryString.replace(/&/g, '","').replace(/=/g,'":"') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value) })
@@ -31,50 +54,85 @@ function utmSaver() {
         }
 
         return result
-    }
+    },
 
-    function parseObjectToQuery(utmModel) {
+    /**
+     * parse query model to query string
+     * @param utmModel
+     * @returns {string}
+     */
+    parseObjectToQuery: function (utmModel) {
         return new URLSearchParams(utmModel.query).toString()
     }
-    
-    // WORKING WITH LOCATION
-    function setQueryParamsToLocation(queryModel) {
-        var currentUrl = new URL(location.href)
+};
 
-        var queryKeys = Object.keys(queryModel)
+/**
+ * Module works with window location (browser history)
+ * @type {{setQueryParamsToLocation: locationModule.setQueryParamsToLocation}}
+ */
+const locationModule = {
+    /**
+     * Set query model to search prop of window location
+     * @param queryModel
+     */
+    setQueryParamsToLocation: function(queryModel) {
+        const currentUrl = new URL(location.href)
 
-        console.log(queryKeys)
+        const queryKeys = Object.keys(queryModel)
 
-        for (var i = 0; i < queryKeys.length; i += 1) {
-            var queryValue = queryModel[queryKeys[i]]
+        for (let i = 0; i < queryKeys.length; i += 1) {
+            const queryKey = queryKeys[i]
+            const queryValue = queryModel[queryKey]
 
-            currentUrl.searchParams.set(queryKeys[i], queryValue)
+            currentUrl.searchParams.set(queryKey, queryValue)
         }
 
         history.pushState({"mt_utm_saver": queryModel}, "MT Query Saver", currentUrl.href)
-    }
+    },
+}
 
-    // WORKING WITH STORAGE
-    function saveQueryToStorage() {
-        var model = utmStorageModel()
+/**
+ *
+ * @type {{getModel: storageModule.getModel, setModel: storageModule.setModel}}
+ */
+const storageModule = {
+    options: {
+        storage: storageTypes.sessionStorage
+    },
 
-        var queryWithoutQuestionMark = location.search.substring(1)
-        var query = parseQueryToObject(queryWithoutQuestionMark)
+    init(options) {
+        this.options = { ...this.options, ...options }
+
+        return this
+    },
+
+    /**
+     * Saves to parsed query to storage
+     * Validate query and query exists
+     * @returns {boolean} success
+     */
+    setModel: function(model) {
+        const queryWithoutQuestionMark = location.search.substring(1)
+        const query = queryModule.parseQueryToObject(queryWithoutQuestionMark) // FIXME: don't use modules inside each other
 
         if (!query) return false
 
         model.query = query
         model.createdAt = Date.now()
 
-        localStorage.setItem(storageKey, JSON.stringify(model))
+        this.getStorage().setItem(storageKey, JSON.stringify(model))
 
         return true
-    }
+    },
 
-    function getFromStorage() {
-        var result = null
+    /**
+     * Get utmModel from storage
+     * @returns {null|*}
+     */
+    getModel: function() {
+        let result = null
 
-        var utmStorageModel = localStorage.getItem(storageKey)
+        const utmStorageModel = this.getStorage().getItem(storageKey)
         if (!utmStorageModel) return null
 
         try {
@@ -85,29 +143,52 @@ function utmSaver() {
         }
 
         return result
-    }
+    },
 
-    // TODO: rewrite option true || false
-    var utmModel = getFromStorage()
+    getStorage: function () {
+        switch (this.options.storage) {
+            case storageTypes.sessionStorage:
+                return sessionStorage
+            case storageTypes.localStorage:
+                return localStorage
+            default:
+                throw Error("Unknown storage type")
+        }
+    }
+}
+
+const defaultOptions = {
+    storage: storageTypes.sessionStorage,
+}
+
+function querySaver(options = defaultOptions) {
+
+    // MODULES INIT
+    const queryM = queryModule // FIXME: work with module inside main instance
+    const locationM = locationModule
+
+    const storageM = storageModule.init(defaultOptions)
+
+    let utmModel = storageM.getModel()
     if (!utmModel) {
-        var saved = saveQueryToStorage()
+        const model = models.newStorageModel()
+
+        const saved = storageM.setModel(model)
         if (!saved) return
     }
 
-    utmModel = getFromStorage()
+    utmModel = storageM.getModel()
 
-    setQueryParamsToLocation(utmModel.query)
+    locationM.setQueryParamsToLocation(utmModel.query)
 }
 
-// url parse +
-// save to localStorage +
-// save and get localstorage +
-// add to window location params +
-// replacing params
-// add only absent params // TODO make param customizable
-// code organize (!)
-// add sub-page +
-// add ttl to save params (?)
+document.addEventListener('DOMContentLoaded', querySaver)
 
-
-document.addEventListener('DOMContentLoaded', utmSaver)
+// TODO:
+// Move everything to window object and close from global scope
+// Add expiration time and option for model to store
+// Add replacement option on/off (global)
+// Add query replacement all / specific tags
+// Add query specific only tags to replace
+// Add links to replace all over the page off / on
+// Add cookies storage
